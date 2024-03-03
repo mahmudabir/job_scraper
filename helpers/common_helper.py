347c16954af994b5396feb9e91d1f13844fbe3ac
Abir <abir@primetechbd.com>
2024-03-03 13:14:14 +0600

    Update common_helper.py

--- helpers/common_helper.py
+++ helpers/common_helper.py
@@ -6,3 +6,13 @@ def wait_for_key():
     # while True:
     #     if msvcrt.kbhit():
     #         return msvcrt.getch()
+
+
+def snake_to_space_case(snake_case_string):
+    # Replace underscores with spaces
+    space_case_string = snake_case_string.replace("_", " ")
+
+    # Capitalize the first letter of each word
+    space_case_string = space_case_string.title()
+
+    return space_case_string
